721af397f726273764d720f97a9434c18067ae43
Igor128157 <igor0063@ukr.net>
2020-02-13 15:13:06 +0200

    add

---
+++ task-04.js
@@ -0,0 +1,26 @@
+const countTotalSalary = function(employees) {
+
+    let sum = 0;
+    for (let salary of Object.values(employees)) {
+      sum += salary;
+    }
+    return sum;
+  };
+
+  console.log(countTotalSalary({}));
+
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  );
+
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  );
